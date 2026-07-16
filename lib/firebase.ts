e1b73a2189357254243ed9c40a9e3c6498d4f857
leeyunseokarchive <dbstjr3576@gmail.com>
2026-07-16 14:44:22 +0900

    feat: add opt-in Firebase emulator connection for local demo capture

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIJw3leCw1Ay1wYBgB7jTgLj8lwZkHuOs",
@@ -15,3 +16,15 @@ const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const functions = getFunctions(app);
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __cosyncEmulatorConnected: boolean | undefined;
+}
+
+if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "1" && !globalThis.__cosyncEmulatorConnected) {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+  globalThis.__cosyncEmulatorConnected = true;
+}
